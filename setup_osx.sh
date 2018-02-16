8c8346564d1da39a12b7b498a7e9911938c68f7c
Derek Roberts <derek.roberts@gmail.com>
2018-02-16 09:11:03 -0800

    setup_osx.sh + a few basic packages

--- setup_osx.sh
+++ setup_osx.sh
@@ -7,3 +7,12 @@ set -eux
 #
 which brew || \
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+# Install packages
+#
+PACKAGES="git ssh wget"
+for p in $PACKAGES
+do
+	which $p || brew install $p
+done
